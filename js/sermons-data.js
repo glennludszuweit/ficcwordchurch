e26b59fdc99b7c97392a4383a305248675f0a123
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2023-09-02 16:06:16 +0100

    first commit

---
+++ js/sermons-data.js
@@ -0,0 +1,14 @@
+let sermonsData = [
+  {
+    title: 'test',
+    url: 'test.com.aksjdbfnsjdkf',
+  },
+  {
+    title: 'test 2',
+    url: 'test.com.aksjdbfnsjdkf',
+  },
+  {
+    title: 'test 3',
+    url: 'test.com.aksjdbfnsjdkf',
+  },
+];
